4a7952b07347d5b54f17e9be80f10fb09d106923
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-03-17 10:30:56 +0630

    php artisan migrated for minimum use

--- resources/views/import_by_queue.blade.php
+++ resources/views/import_by_queue.blade.php
@@ -16,7 +16,7 @@
                     <div class="basis-1/2 border mx-auto my-4 flex flex-col "> 
                         <div class=" border m-2">
                             <label> Import file : </label> <br/>
-                            <input type="file" class="file:border file:border-solid ..." />
+                            <input name="importingFile"  type="file" class="file:border file:border-solid ..." />
                         </div>
                         <div class="border m-2 flex justify-center">
                             <button type="submit" class="rounded bg-blue-400 p-2"> Import </button>
